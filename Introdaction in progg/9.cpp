e68ad2d3fb8f6461089d51388b7a6d907553c427
LebArt2321 <LebArt2321@github.com>
2020-09-24 15:49:05 +0300

    добавлена 8 и 9 лабораторная работа

---
+++ Introdaction in progg/9.cpp
@@ -0,0 +1,44 @@
+/*#include<iostream>
+#include<math.h>
+using namespace std;*/
+//1//
+/*int main() {
+	int time;
+	cin >> time;
+	cout << time % 60;
+}*/
+//2//
+/*int main() {
+	int k;
+	cin >> k;
+	cout << k % 7;
+}*/
+//3//
+/*int main() {
+	int k, n;
+	cin >> k;
+	cin >> n;
+	int x = (k + n) % 7;
+	if (x - 1 == 0) cout << 7;
+	else 
+	cout << x-1;
+}*/
+//4//
+/*int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	int A = a / c;
+	int B = b / c;
+	int S = A * B;
+	int s = S / c * c;
+	cout << s;
+	int ost = a * b - (A*c*B*c);
+	cout << "\n" << ost;	
+}*/
+//5//
+/*int main() {
+	int x;
+	cin >> x;
+	cout << x / 100;
+}*/
+
