c6a83e51953b0e6a2d407cc3a1bd195da6ecd440
LebArt2321 <LebArt2321@github.com>
2020-11-25 15:26:27 +0300

    Добавлена 14 лабораторная работа

---
+++ Introdaction in progg/14.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+//1//
+int main()
+{
+	int a, b, k;
+	cout << "Enter A<B:";
+	cin >> a >> b;
+	for (int i = a; i <= b; i++) {
+		k = a;
+		while (k > 0) {
+			cout << a << " ";
+			k--;
+		}
+		cout << endl;
+		a++;
+	}
+	return 0;
+}
+//2//
+int main()
+{
+	int a, b;
+	cout << "Enter A>B:";
+	cin >> a >> b;
+	while (a > 1) {
+		a -= b;
+	}
+	if (a < 0) { cout << a + b; }
+	else cout << a;
+	return 0;
+}
+//3//
+int main()
+{
+	int n, k = 0, s = 0;
+	cout << "Enter n:";
+	cin >> n;
+	while (k <= n) {
+		s = s + k;
+		k++;
+	}
+	cout << k - 1 << '\n';
+	cout << s;
+	return 0;
+}
+//4//
+int main()
+{
+	int s = 1000, k = 0;
+	double p, r;
+	cout << "Enter P:";
+	cin >> p;
+	while (s <= 1100) {
+		r = p * s / 100 + s;
+		s = r;
+		k++;
+	}
+	cout << "k=" << k << endl;
+	cout << "s=" << s;
+	return 0;
+}
+//5//
+int main()
+{
+	int a, b;
+	cout << "Enter a,b:";
+	cin >> a >> b;
+	while (a % b != 0) {
+		a = a % b;
+		swap(a, b);
+	}
+	cout << b;
+	return 0;
+}
+
+//6//
+int main()
+{
+	int n, f = 0, k = 2, x = 0, y = 1;
+	cout << "Enter N:";
+	cin >> n;
+	while (f != n) {
+		f = x + y;
+		x = y;
+		y = f;
+		k++;
+	}
+	cout << "k=" << k;
+	return 0;
+}
